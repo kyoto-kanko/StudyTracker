b8df8d7daed6f193c73720ed33321dfb9d09ec43
kyoto-kanko <kyoto.kanko.biz@gmail.com>
2023-09-08 22:47:58 +0900

    first commit

---
+++ src/test/java/com/example/StudyTracker/StudyTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StudyTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
